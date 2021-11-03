e0b0e43f4fe56f232d633ff46b0e50b99c9d19e5
shizhongqi <shishaoqi0928@gmail.com>
2021-11-03 22:19:58 +0800

    1. 01 - Hello World 2. 02 - Values

---
+++ 01 - Hello World/helloWorld.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+// Sometimes we'll want to build our programs into binaries.
+// We can do this using: go build xxxx.go
+func main() {
+	fmt.Println("hello world")
+}
