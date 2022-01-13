7cbb27e36b8b5cef91dc6333f3f897fdc78a3194
shizhongqi <shishaoqi0928@gmail.com>
2022-01-13 23:51:09 +0800

    59 - Reading Files -- finish halfly

---
+++ 59 - Reading Files/reading_files.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func main() {
+	dat, err := ioutil.ReadFile("59 - Reading Files/dat")
+	check(err)
+	fmt.Println(string(dat))
+
+	f, err := os.Open("59 - Reading Files/dat")
+	check(err)
+
+	b1 := make([]byte, 5)
+	n1, err := f.Read(b1)
+	check(err)
+	fmt.Printf("%d bytes: %s\n", n1, string(b1[:n1]))
+}
