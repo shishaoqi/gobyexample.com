f0a3d77f3a109845e8142bc71de07795b7addee3
shizhongqi <shishaoqi0928@gmail.com>
2022-01-30 15:19:42 +0800

    69 - Envronment Variables

---
+++ 69 - Envronment Variables/environment_variables.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	os.Setenv("FOO", "1")
+	fmt.Println("FOO:", os.Getenv("FOO"))
+	fmt.Println("BAR:", os.Getenv("BAR"))
+
+	fmt.Println()
+	fmt.Println(os.Environ())
+	fmt.Println()
+	for _, e := range os.Environ() {
+		pair := strings.SplitN(e, "=", 2)
+		fmt.Println(pair[0])
+		fmt.Printf("%v = %v\n", pair[0], pair[1])
+	}
+}
