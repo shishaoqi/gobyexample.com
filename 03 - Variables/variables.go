549d6726fafe10b2ffca6bb11725e72be57c0c71
shizhongqi <shishaoqi0928@gmail.com>
2022-09-14 20:09:54 +0800

    添加注释，加深理解

--- 03 - Variables/variables.go
+++ 03 - Variables/variables.go
@@ -6,13 +6,13 @@ func main() {
 	var a = "initial"
 	fmt.Println(a)
 
-	var b, c int = 1, 2
+	var b, c int = 1, 2 // int 可省略
 	fmt.Println(b, c)
 
-	var d = true
+	var d = true // 省略了标识类型符 bool
 	fmt.Println(d)
 
-	var e int
+	var e int // 没有初始变量的值时，则必须在变量名后加上类型，此时变量会默认加上该类型的初始值
 	fmt.Println(e)
 
 	// The := syntax is shorthand for declaring and initializing a varible
