2d52991ca8ec47fa216562574119a56b864dffba
shizhongqi <shishaoqi0928@gmail.com>
2022-12-31 10:45:43 +0800

    温习 01 - Hello World

--- 01 - Hello World/helloWorld.go
+++ 01 - Hello World/helloWorld.go
@@ -2,8 +2,11 @@ package main
 
 import "fmt"
 
+// To run the programm, put the code in helloWorld.go and use go run helloWorld.go
+
 // Sometimes we'll want to build our programs into binaries.
 // We can do this using: go build xxxx.go
+
 func main() {
 	fmt.Println("hello world")
 }
