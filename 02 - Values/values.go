3af4808eaa048501691a16358154aee03cff340c
shizhongqi <shishaoqi0928@gmail.com>
2022-12-31 11:03:44 +0800

    2022.12.31 温习 01 - Values

--- 02 - Values/values.go
+++ 02 - Values/values.go
@@ -5,8 +5,8 @@ import "fmt"
 func main() {
 	fmt.Println("go" + "lang") // Strings, which can be added together with +.
 
-	fmt.Println("1+1 = ", 1 + 1)
-	fmt.Println("7.0/3.0 = ", 7.0/3.0)
+	fmt.Println("1+1 = ", 1+1)         // Integers
+	fmt.Println("7.0/3.0 = ", 7.0/3.0) // Floats
 
 	// Booleans, with boolean operators as you'd expect
 	fmt.Println(true && false)
